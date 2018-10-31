83909e2e8ea3db2881700a9f5f66e1f2afcf85ba
Mahi97 <mohammadmahdi76@gmail.com>
2018-10-31 12:23:07 -0400

    remove unused argument of timeneeded function and remove unused methods

--- parsian_agent/include/parsian_agent/gotopointavoid.h
+++ parsian_agent/include/parsian_agent/gotopointavoid.h
@@ -28,7 +28,7 @@ protected:
 public:
     Vector2D ballPos;
     void init(Vector2D _target, Vector2D _targetDir, Vector2D _targetVel = Vector2D(0.0, 0.0));
-    static double timeNeeded(Agent *_agentT, Vector2D posT, double vMax, QList <int> _ourRelax, QList <int> _oppRelax , bool avoidPenalty, double ballObstacleReduce, bool _noAvoid);
+    static double timeNeeded(Agent *_agentT, Vector2D posT, double vMax);
     DEF_SKILL(CSkillGotoPointAvoid);
     CSkillGotoPointAvoid* noRelax();
     CSkillGotoPointAvoid* ourRelax(int element);
