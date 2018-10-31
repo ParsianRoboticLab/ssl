26a1c81bdc2dc1b10655ffc11af7ae485c3c24d0
Mahi97 <mohammadmahdi76@gmail.com>
2018-10-31 12:20:31 -0400

    remove CSKills and unused methods from CSkill

--- parsian_agent/include/parsian_agent/skill.h
+++ parsian_agent/include/parsian_agent/skill.h
@@ -19,71 +19,17 @@ using namespace rcsc;
 
 class CSkill {
 public:
-    QString localAgentName;
-    CSkill() = default;
-
     explicit CSkill(Agent* _agent);
     ~CSkill();
-    virtual CSkill* allocate(Agent* _agent) = 0;
-    virtual QString getName() = 0;
-
     //these functions should be defined in child classes
-    virtual double progress() = 0; //between 0-1 ; less than zero on failure
     virtual void execute() = 0;
 
     Property(Agent*, Agent, agent){};
-    friend class CSkills;
-};
-
-class CSkills {
-public:
-    CSkills();
-    ~CSkills();
-    static bool registerSkill(const char *name, CSkill* Skill);
-    static int skillsCount();
-    static CSkill* skill(int i);
-private:
-    struct RegisteredSkill {
-        const char *name;
-        CSkill *Skill;
-        void* Info;
-    };
-    static QList<RegisteredSkill>* Skills;
-    static bool inited;
 };
 
 #define DEF_SKILL(skill) \
     skill(Agent* _agent); \
-    skill() {} \
     ~skill(); \
-    static const char *Name;\
-    virtual CSkill* allocate(Agent* _agent); \
-    virtual void execute(); \
-    virtual double progress(); \
-    virtual QString getName(); \
-    static skill* set(Agent* a); \
-    static skill* get(Agent* a)
+    virtual void execute();
 
-
-#define INIT_SKILL(_Skill,name) \
-    CSkill* _Skill::allocate(Agent* _agent) \
-    {return new _Skill(_agent);} \
-    QString _Skill::getName() {return QString(Name);} \
-    _Skill* _Skill::set(Agent* a) \
-    { \
-        if (QString(_Skill::Name)!=a->skillName) \
-        { \
-            if (a->skill!=NULL && a->skillName!="") delete ((_Skill*) a->skill); \
-            else a->skill = (CSkill*) (new _Skill(a)); \
-            a->skillName = _Skill::Name; \
-        } \
-        return (_Skill*) a->skill; \
-    } \
-    _Skill* _Skill::get(Agent* a) \
-    { \
-        return (_Skill*) a->skill; \
-    } \
-        bool _Skill##_registered \
-      = CSkills::registerSkill(_Skill::Name,new _Skill());\
-    const char* _Skill::Name = name
 #endif // BASICSKILL_H
