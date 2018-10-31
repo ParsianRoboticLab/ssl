f0b25615f8437cfcccb6ecd20edfe35be672e9cd
Mahi97 <mohammadmahdi76@gmail.com>
2018-10-31 16:32:09 -0400

    clean and refactor onetouch skill

--- parsian_agent/include/parsian_agent/onetouch.h
+++ parsian_agent/include/parsian_agent/onetouch.h
@@ -4,19 +4,31 @@
 #include <parsian_agent/skill.h>
 #include <parsian_agent/kick.h>
 #include <parsian_util/action/autogenerate/onetouchaction.h>
+#include <parsian_agent/receivepass.h>
+
+enum class OTMode {
+    None = 0,
+    Wait = 1,
+    Kick = 2,
+    Intersect = 3
+};
 
 class CSkillKickOneTouch : public CSkill, public OnetouchAction {
-protected:
-    CSkillGotoPointAvoid* gotopointavoid;
-    CSkillKick* kick;
-    QTime* timeAfterForceKick;
 private:
     Vector2D findMostPossible();
-    Vector2D ballLastVel;
-    Vector2D lastInterceptPos;
+    OTMode decideMode();
+    void wait();
+    void kick();
+    void intersect();
+    void validatePoint(Vector2D& _point);
+    CSkillGotoPointAvoid* gotopointavoid;
+    CSkillKick* kickSkill;
+    QTime* timeAfterForceKick;
 public:
-    static double oneTouchAngle(Vector2D pos, Vector2D vel, Vector2D ballVel, Vector2D ballDir, Vector2D goal, double landa, double gamma,double vkick);
-    DEF_SKILL(CSkillKickOneTouch);
+    explicit CSkillKickOneTouch(Agent* _agent);
+    ~CSkillKickOneTouch();
+    void execute() override;
+    static double oneTouchAngle(Vector2D pos, Vector2D vel, Vector2D ballVel, Vector2D ballDir, Vector2D goal, double lambda, double gamma,double vkick);
 };
 
 #endif //PARSIAN_SKILLS_ONETOUCH_H
--- parsian_agent/src/parsian_agent/onetouch.cpp
+++ parsian_agent/src/parsian_agent/onetouch.cpp
@@ -2,55 +2,51 @@
 // Created by parsian-ai on 9/21/17.
 //
 
-#include "parsian_agent/onetouch.h"
+#include <parsian_agent/onetouch.h>
+
 
 
 CSkillKickOneTouch::CSkillKickOneTouch(Agent *_agent) : CSkill(_agent) {
     gotopointavoid = new CSkillGotoPointAvoid(agent);
-    gotopointavoid->setAgent(_agent);
-    kick = new CSkillKick(_agent);
+    kickSkill = new CSkillKick(_agent);
     timeAfterForceKick = new QTime();
     timeAfterForceKick->start();
-    waitPos.invalidate();
-    kickSpeed = 800;
-    chip = false;
 }
 
 CSkillKickOneTouch::~CSkillKickOneTouch() {
     delete gotopointavoid;
-    delete kick;
+    delete kickSkill;
     delete timeAfterForceKick;
 }
 
 double CSkillKickOneTouch::oneTouchAngle(Vector2D pos,
-        Vector2D vel,
-        Vector2D ballVel,
-        Vector2D ballDir,
-        Vector2D goal,
-        double landa,
-        double gamma,
-        double vkick) {
-    float ang1 = (-ballDir).th().degree();
-    float ang2 = (goal - pos).th().degree();
-    float theta = AngleDeg::normalize_angle(ang2 - ang1);
-    float th = fabs(theta) * _DEG2RAD;
-     float v = (ballVel - vel).length();
-    float th1 = th * 0.5;
-    float f, fmin = 1e10;
-    float th1best;
+                                         Vector2D vel,
+                                         Vector2D ballVel,
+                                         Vector2D ballDir,
+                                         Vector2D goal,
+                                         double lambda,
+                                         double gamma,
+                                         double vkick) {
+    double ang1 = (-ballDir).th().degree();
+    double ang2 = (goal - pos).th().degree();
+    double theta = AngleDeg::normalize_angle(ang2 - ang1);
+    double th = fabs(theta) * _DEG2RAD;
+    double v = (ballVel - vel).length();
+    double th1;
+    double f, f_min = 1e10;
+    double th1best{};
     for (int k = 0; k < 6000; k++) {
-        th1 = ((float)k / 6000.0) * th;
-        f  = gamma * v * (1.0 / tan(th - th1)) * sin(th1) - landa * v * cos(th1) - vkick;
-        if (fabs(f) < fmin) {
-            fmin = fabs(f);
+        th1 = (k / 6000.0) * th;
+        f  = gamma * v * (1.0 / tan(th - th1)) * sin(th1) - lambda * v * cos(th1) - vkick;
+        if (fabs(f) < f_min) {
+            f_min = fabs(f);
             th1best = th1;
         }
     }
     th1 = th1best;
     th1 *= _RAD2DEG;
     AngleDeg::normalize_angle(th1);
-    th  *= _RAD2DEG;
-    float ang = 0;
+    double ang = 0;
     if (theta > 0) {
         ang = ang1 + th1;
     } else {
@@ -63,162 +59,113 @@ double CSkillKickOneTouch::oneTouchAngle(Vector2D pos,
 
 Vector2D CSkillKickOneTouch::findMostPossible() {
 
-    QList<int> tempObstacles;
     QList <Circle2D> obstacles;
-    obstacles.clear();
     for (int i = 0 ; i < wm->opp.activeAgentsCount() ; i++) {
-        obstacles.append(Circle2D(wm->opp.active(i)->pos, 0.1));
+        obstacles.append(Circle2D(wm->opp.active(i)->pos, Robot::robot_radius_new + 0.01));
     }
-
     for (int i = 0 ; i < wm->our.activeAgentsCount() ; i++) {
         if (wm->our.active(i)->id != agent->id()) {
-            obstacles.append(Circle2D(wm->our.active(i)->pos, 0.1));
+            obstacles.append(Circle2D(wm->our.active(i)->pos, Robot::robot_radius_new + 0.01));
         }
     }
+
     double prob, angle, biggestAngle;
-
     CKnowledge::getEmptyAngle(*wm->field, wm->ball->pos - (wm->field->oppGoal() - wm->ball->pos).norm() * 0.15, wm->field->oppGoalL(), wm->field->oppGoalR(), obstacles, prob, angle, biggestAngle);
-    DEBUG(QString("prob: %1 , angle :%2, biggest:%3").arg(prob).arg(angle).arg(biggestAngle), D_MHMMD);
-
     Segment2D goalSeg(wm->field->oppGoalL(), wm->field->oppGoalR());
-    Vector2D sol1, sol2;
-    DEBUG(QString("ang %1").arg(angle), D_MHMMD);
-    drawer->draw(Segment2D(wm->ball->pos , wm->ball->pos + Vector2D(cos(_PI * (angle) / 180), sin(_PI * (angle) / 180)).norm() * 12));
-
-    return  goalSeg.intersection(Segment2D(wm->ball->pos , wm->ball->pos + Vector2D(cos(_PI * (angle) / 180), sin(_PI * (angle) / 180)).norm() * 12));
+    return goalSeg.intersection(Segment2D(wm->ball->pos , wm->ball->pos + Vector2D(cos(_PI * (angle) / 180), sin(_PI * (angle) / 180)).norm() * 12));
 }
 
 
 void CSkillKickOneTouch::execute() {
-    gotopointavoid->setAgent(agent);
     gotopointavoid->setOnetouchmode(false);
     gotopointavoid->setNoavoid(false);
 
-    if (shotToEmptySpot) {
-        target = findMostPossible();
+    if (shotToEmptySpot) target = findMostPossible();
+    if (!target.valid()) target = wm->field->oppGoal();
+    if (!waitPos.isValid()) waitPos = agent->pos();
+
+    OTMode mode = decideMode();
+
+    switch (mode) {
+        case OTMode::None:
+        case OTMode::Wait:
+            wait();
+            break;
+        case OTMode::Kick:
+            kick();
+            break;
+        case OTMode::Intersect:
+            intersect();
+            break;
     }
-    if (!target.valid()) {
-        target = wm->field->oppGoal();
-    }
 
+}
 
+OTMode CSkillKickOneTouch::decideMode() {
 
-    Vector2D ballPos = wm->ball->pos;
-    Vector2D agentPos = agent->pos();
-
-    Segment2D ballPath;
-    double stopParam = 0.085;
-    ballPath.assign(ballPos, ballPos + wm->ball->vel.norm() * 15);
-    Segment2D ballLine;
-    ballLine.assign(ballPos, ballPos + wm->ball->vel.norm() * (15));
-    drawer->draw(ballPath, "red");
-    Vector2D kickerPoint = agentPos + agent->dir().norm() * stopParam;
-
-
-
-    Vector2D oneTouchDir = Vector2D::unitVector(oneTouchAngle(agentPos, agent->vel(), wm->ball->vel, agentPos - ballPos, target, conf->Landa, conf->Gamma,6.5));
-    drawer->draw(QString("vball :%1").arg(conf->Landa), Vector2D(0, 0));
-    //drawer->draw(Segment2D(Vector2D(0,0), Vector2D(0,0)+oneTouchDir.norm()), QColor(Qt::red));
-
-    Vector2D intersectPos;
-    Vector2D sol1, sol2;
-    double onetouchRad = 2;
     double onetouchKickRad = 0.5;
+    double stopParam = 0.085;
+    double onetouchRad = std::min(2.0, wm->ball->pos.dist(agent->pos()) - stopParam);
 
-    Vector2D addVec ;
-    Circle2D oneTouchArea;
-    Circle2D oppPenaltyArea(wm->field->oppGoal() + Vector2D(0.15, 0), 1.45);
-    Rect2D oppPenaltyAreaWP = wm->field->oppBigPenaltyArea(1,0.1,0);
-    drawer->draw(oppPenaltyAreaWP, QColor(Qt::red));
-    drawer->draw(oppPenaltyArea, QColor(Qt::red));
+    Circle2D oneTouchArea(agent->pos(), onetouchRad);
+    Segment2D ballPath(wm->ball->pos, wm->ball->pos + wm->ball->vel.norm() * 15);
 
-    if (ballPos.dist(agentPos) <= onetouchRad) {
-        onetouchRad = ballPos.dist(agentPos) - stopParam;
-    }
-    oneTouchArea.assign(agentPos, onetouchRad);
-
-    if ((wm->ball->vel.length() < 0.4 && agentPos.dist(ballPos) > onetouchKickRad) || ((oneTouchArea.intersection(ballPath, &sol1, &sol2) ==
-            0) && wm->ball->vel.length() >= 0.4 && agentPos.dist(ballPos) > onetouchKickRad)) {
-        if (!waitPos.isValid()) {
-            waitPos = agentPos;
-        }
-        gotopointavoid->init(waitPos, oneTouchDir);
-        gotopointavoid->execute();
-        agent->setRoller(0);
-    } else if ((oneTouchArea.intersection(ballPath, &sol1, &sol2) != 0) && wm->ball->vel.length() > 0.1) {
-        gotopointavoid->setNoavoid(true);
-        double agentTime = 0 ;
-        drawer->draw(QString("agentT dif : %1").arg(kickerPoint.dist(wm->ball->getPosInFuture(0.5))) , Vector2D(2.5, -1));
-        Vector2D dummy;
-        if (!fastestPoint || Circle2D(agentPos, 0.1).intersection(Segment2D(wm->ball->pos, wm->ball->getPosInFuture(reachBeforeBallTime)), &dummy, &dummy)) {
-            intersectPos = ballPath.nearestPoint(kickerPoint);
-        } else {
-            bool posFound  = false;
-            for (double i = 0 ; i < 5 ; i += 0.1) {
-
-                intersectPos = wm->ball->getPosInFuture(i);// - (target-wm->ball->getPosInFuture(i)).norm()*0.15;
-                QList <int> dummy;
-                (intersectPos - target).norm() * stopParam;
-                agentTime = CSkillGotoPointAvoid::timeNeeded(agent, intersectPos + addVec, conf->VelMax);
-
-
-                if (agentTime < (i - (reachBeforeBallTime))) {
-                    posFound  = true;
-                    break;
-                }
-            }
-
-            if (posFound == false || /*intersectPos.dist(ballPos) > ballPath.nearestPoint(kickerPoint).dist(ballPos) ||*/ !wm->field->isInField(intersectPos + addVec)) {
-                intersectPos = ballPath.nearestPoint(kickerPoint);
-            }
-        }
-        //        intersectPos = ballPath.nearestPoint(kickerPoint);
-        drawer->draw(QString("agentT : %1").arg(agentTime) , Vector2D(1, -1));
-
-
-        if(intersectPos.x >wm->field->_FIELD_WIDTH/2 -  wm->field->_PENALTY_DEPTH - 0.1 && fabs(intersectPos.y) < wm->field->_PENALTY_WIDTH/2 +0.1 ) {
-            if(wm->field->oppBigPenaltyArea(1,0.1,0).intersection(ballPath,&sol1,&sol2)) {
-                if(sol1.dist(ballPos)  < sol2.dist(ballPos)) {
-                    if(sol2.x != wm->field->oppGoal().x) {
-                        sol1 = sol2;
-                    }
-                }
-                if(sol1.x == wm->field->oppGoal().x)
-                    sol1 = sol2;
-                intersectPos = sol1;
-            }
-        }
-
-        Vector2D addVec = (intersectPos - target).norm() * stopParam;
-
-        gotopointavoid->init(intersectPos + addVec, oneTouchDir);
-        gotopointavoid->setNoavoid(true);
-        gotopointavoid->setOnetouchmode(true);
-        gotopointavoid->execute();
-        drawer->draw(intersectPos);
-        if (agentPos.dist(ballPos) < 1) {
-            if (chip) {
-                agent->setChip(kickSpeed);
-            } else {
-                agent->setKick(kickSpeed*0.8);
-            }
-        }
-        agent->setRoller(0);
-    } else if (ballPos.dist(agentPos) < onetouchKickRad) {
-        kick->setAgent(agent);
-        kick->setTarget(target);
-        kick->setKickspeed(kickSpeed*0.8);
-        kick->setChip(chip);
-        kick->execute();
+    if (oneTouchArea.intersection(ballPath) && wm->ball->vel.length() > 0.25) {
+        return OTMode::Intersect;
+    } else if (wm->ball->pos.dist(agent->pos()) < onetouchKickRad) {
+        return OTMode::Kick;
     } else {
-        if (!waitPos.isValid()) {
-            waitPos = agentPos;
-        }
-        gotopointavoid->init(waitPos, oneTouchDir);
-        gotopointavoid->execute();
+        return OTMode::Wait;
+    }
+}
+
+void CSkillKickOneTouch::wait() {
+    Vector2D oneTouchDir = Vector2D::unitVector(oneTouchAngle(agent->pos(), agent->vel(), wm->ball->vel, agent->pos() - wm->ball->pos, target, conf->Landa, conf->Gamma,6.5));
+    gotopointavoid->init(waitPos, oneTouchDir);
+    gotopointavoid->execute();
+    agent->setRoller(0);
+}
+
+void CSkillKickOneTouch::kick() {
+    kickSkill->setTarget(target);
+    kickSkill->setKickspeed(kickSpeed);
+    kickSkill->setChip(chip);
+    kickSkill->execute();
+}
+
+void CSkillKickOneTouch::intersect() {
+    Segment2D ballPath(wm->ball->pos, wm->ball->pos + wm->ball->vel.norm() * 15);
+    Vector2D intersectPos = CSkillReceivePass::bestPointToIntersect(agent, reachBeforeBallTime);
+    double stopParam = 0.085;
+    Vector2D kickerPoint = agent->pos() + agent->dir().norm() * stopParam;
+    if (!intersectPos.valid() || !fastestPoint || Circle2D(agent->pos(), 0.15).intersection(Segment2D(wm->ball->pos, wm->ball->getPosInFuture(reachBeforeBallTime)))) {
+        intersectPos = ballPath.nearestPoint(kickerPoint);
     }
 
-   // agent->setKick(600);
-    ballLastVel = wm->ball->vel;
-    lastInterceptPos = intersectPos;
+    validatePoint(intersectPos);
+
+    Vector2D addVec = (intersectPos - target).norm() * stopParam;
+    Vector2D oneTouchDir = Vector2D::unitVector(oneTouchAngle(agent->pos(), agent->vel(), wm->ball->vel, agent->pos() - wm->ball->pos, target, conf->Landa, conf->Gamma,6.5));
+    gotopointavoid->init(intersectPos + addVec, oneTouchDir);
+    gotopointavoid->setNoavoid(true);
+    gotopointavoid->setOnetouchmode(true);
+    gotopointavoid->setNoavoid(true);
+    gotopointavoid->execute();
+    drawer->draw(intersectPos);
+
+    if (agent->pos().dist(wm->ball->pos) < 1) {
+        if (chip) agent->setChip(kickSpeed);
+        else agent->setKick(kickSpeed);
+    }
+    agent->setRoller(0);
+}
+
+void CSkillKickOneTouch::validatePoint(Vector2D &_point) {
+    const Rect2D& biggerOppPenalty = wm->field->oppBigPenaltyArea(1, Robot::robot_radius_new, false);
+    const Rect2D& biggerOurPenalty = wm->field->ourBigPenaltyArea(1, Robot::robot_radius_new, false);
+
+    if (biggerOppPenalty.contains(_point)) CSkillReceivePass::validatePointFromPenaltyWithTarget(_point, biggerOppPenalty, waitPos);
+    else if (biggerOurPenalty.contains(_point)) CSkillReceivePass::validatePointFromPenaltyWithTarget(_point, biggerOurPenalty, waitPos);
+    else if (!wm->field->fieldRect().contains(_point)) CSkillReceivePass::validatePointOutofField(_point);
+
 }
