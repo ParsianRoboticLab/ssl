c09d42611552ef72165459087261e0ef48335eb7
hamidreza hemati <hrhhemati@gmail.com>
2018-11-05 20:31:22 +0330

    pusher and holder placement done ...just pushing the ball to desierd pos remain and isKickerOnThePosition function must fixed

--- parsian_ai/include/parsian_ai/plays/ourballplacement.h
+++ parsian_ai/include/parsian_ai/plays/ourballplacement.h
@@ -14,7 +14,7 @@ enum class BallPlacement {
     DONE = 7
 };
 
-class COurBallPlacement : public CMasterPlay{
+class COurBallPlacement : public CMasterPlay {
 public:
         COurBallPlacement();
         ~COurBallPlacement();
@@ -23,40 +23,31 @@ public:
         bool first;
         static int chooseFirst();
 
-
 private:
-        void reset() override;
-        void otherRobotsFormation(const int&) const;
-        int agentFinder(const Vector2D& pos , int) const;
-        int firstStep(const Vector2D&);
-        double xFinder(const Vector2D& desired , const Vector2D& ballPos , const double& dist) const ;
-        double yFinder(const Vector2D& desired , const Vector2D& ballPos , const double& dist) const ;
-        double tetaFinder(const Vector2D& desired, const Vector2D& ballPos) const ;
-        BallPlacement state;
-        bool flag;
-        Vector2D passballpos;
-        int minIndexPos;
-        CAgent *ap;
-        int nearAgentToBall;
-        CAgent *a;
-        double array[2][2];
-        bool nearFlag , phFlag , shotFlag ,updateFlag ,reciveFlag;
-        int nearID , nearTargetAgent;
-        Vector2D currentBallPos;
-        Vector2D lastBallPos;
-        Vector2D ballPosBeforKick;
-        GotopointavoidAction* gpa;
-        GotopointavoidAction* gpa0;GotopointavoidAction* gpa1;GotopointavoidAction* gpa2;GotopointavoidAction* gpa3;
-        GotopointavoidAction* gpa4;GotopointavoidAction* gpa5;GotopointavoidAction* gpa6;;GotopointavoidAction* gpa7;
-        ReceivepassAction* recivePass;
-        KickAction* pass;
-        bool ballHaseMoved(const Vector2D& , const Vector2D&) const;
-        bool ballDidntAriveToTarget(const Vector2D& ballPos , const Vector2D& desiredPos , const Vector2D& ballPosBeforKick) const;
-        bool ballDidntKickedWell(const Vector2D& ballPos, const Vector2D& ballPosBeforKick) const;
-        bool reciverAgentIsOnThePosition(const int &agent, const Vector2D &targetPos) const;
-        bool ballIsNearToTarget(const Vector2D &ballPos, const Vector2D &targetPos) const ;
-        bool ballSpeedIsLow();
-        bool kickerAgentIsNearToBall(const Vector2D &ballPos, const int &agent) const ;
+    int kickerAgent;
+    int receiverAgent;
+    bool nearFlag, shotFlag, updateFlag , loop;
+    int nearID ;
+    Vector2D lastBallPos;
+    Vector2D ballPosBeforKick;
+    GotopointavoidAction *gpaP;
+    GotopointavoidAction *gpaH;
+    GotopointavoidAction *gpa[_NUM_PLAYERS];
+    ReceivepassAction *recivePass;
+    KickAction *pass;
+
+
+    static bool isBallHaseMoved(const Vector2D &, const Vector2D &);
+    static bool isBallDidntKickedWell(const Vector2D &ballPos, const Vector2D &ballPosBeforKick);
+    bool isReciverAgentOnThePosition(const int &agent, const Vector2D &targetPos);
+    static bool isBallNearToTarget(const Vector2D &ballPos, const Vector2D &targetPos, const double &dist);
+    bool isBallSpeedLow(const double &speed, const Vector2D &velocity);
+    bool isKickerOnThePosition(const Vector2D &ballPos, const int &agent) const;
+    bool isPassReceived(const Vector2D &ballPos, const Vector2D &desiredPos, const int &nearAgent, const int &nearTargetAgent);
+    void reset() override;
+    void otherRobotsFormation(const int & ,  const int&) const;
+    int agentFinder(const Vector2D &, const int &);
+    int firstStep(const Vector2D &);
 };
 
 #endif // OURBALLPLACEMENT_H
