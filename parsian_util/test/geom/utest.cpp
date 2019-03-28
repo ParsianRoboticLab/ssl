3b8cf394af5b4d23c8fe08a80d36e30f304912f6
raziyeh <raziyehmohajer.rms@gmail.com>
2019-03-28 19:38:36 +0430

    Change polygon_2d tests

--- parsian_util/test/geom/utest.cpp
+++ parsian_util/test/geom/utest.cpp
@@ -1,14 +1,43 @@
-//
-// Created by raziyeh on 3/17/19.
-//
+#include <parsian_util/geom/polygon_2d.h>
+#include <parsian_util/geom/vector_2d.h>
+#include <parsian_util/geom/rect_2d.h>
+
 #include <gtest/gtest.h>
-#include <parsian_util/geom/angle_deg.h>
+#include <ros/ros.h>
 
 using namespace rcsc;
 
-TEST(AngleDeg, AngleDeg_AngleDeg_Test){
 
+// Declare a test
+TEST(Polygon2DTest, polygon2DTest) {
+
+    ///
+    const Polygon2D empty_polygon;
+    EXPECT_TRUE( !empty_polygon.contains( rcsc::Vector2D( 0.0, 0.0 ) ));
+
+    //
+    const Vector2D p( +100.0, +100.0 );
+
+    std::vector< rcsc::Vector2D > v;
+    v.push_back( p );
+
+    const rcsc::Polygon2D point_polygon( v );
+
+    EXPECT_TRUE( !point_polygon.contains( rcsc::Vector2D( 0.0, 0.0 ) ) );
+
+    // strict checks
+    EXPECT_TRUE(  point_polygon.contains( p ) );
+    EXPECT_TRUE( !point_polygon.contains( p, false ) );
+
+}
+TEST(TestMatrix2D, testMatrix2D){
 
 }
 
-
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv){
+    testing::InitGoogleTest(&argc, argv);
+    ros::init(argc, argv, "tester");
+    ros::NodeHandle nh;
+    return RUN_ALL_TESTS();
+}
