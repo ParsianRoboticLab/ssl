6da0b04e6c0ad73caf2be772d10984b9adbd503a
Raziyeh Mohajer <raziyehmohajer.rms@gmail.com>
2019-03-28 11:08:53 +0430

    Add angle_deg test

--- parsian_util/test/geom/utest.cpp
+++ parsian_util/test/geom/utest.cpp
@@ -1,4 +1,14 @@
 //
 // Created by raziyeh on 3/17/19.
 //
+#include <gtest/gtest.h>
+#include <parsian_util/geom/angle_deg.h>
+
+using namespace rcsc;
+
+TEST(AngleDeg, AngleDeg_AngleDeg_Test){
+
+
+}
+
 
